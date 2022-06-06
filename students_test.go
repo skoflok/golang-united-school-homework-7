48ca85c020a2c99c12d863dfbfbb24c61f99df6d
skoflok <skoflok@gmail.com>
2022-06-06 20:32:19 +0400

    New func cover

--- students_test.go
+++ students_test.go
@@ -75,3 +75,56 @@ func Test_Less(t *testing.T) {
 		}
 	}
 }
+
+func Test_Swap(t *testing.T) {
+	p := testPeople()
+	p0 := p[0]
+	p3 := p[3]
+
+	p.Swap(0, 3)
+
+	if p[0] == p0 && p[3] == p3 {
+		t.Errorf("Persons not been swaped")
+	}
+}
+
+func Test_SimpleNew(t *testing.T) {
+	simpleInput := "11 12\n21 22"
+	simpleMatrix := Matrix{2, 2, []int{11, 12, 21, 22}}
+
+	dataProvider := map[string]struct {
+		input  string
+		output Matrix
+		err    error
+	}{
+		"simple": {
+			input:  simpleInput,
+			output: simpleMatrix,
+			err:    nil,
+		},
+	}
+
+	for k, data := range dataProvider {
+		_, err := New(data.input)
+		if err != nil {
+			t.Errorf("%s. Matrix's error is not be nil", k)
+		}
+	}
+
+}
+
+func Test_AoitErrorNew(t *testing.T) {
+	atoiErrorInput := "11A 12\n21 22"
+	_, err := New(atoiErrorInput)
+	if err == nil {
+		t.Errorf("Matrix's error is nil. Error should be Atoi error")
+	}
+}
+
+func Test_DiffrentRowsLengthNew(t *testing.T) {
+	simpleInput := "11 12\n21 22\n 31"
+	_, err := New(simpleInput)
+	if err == nil {
+		t.Errorf("Matrix's error is nil. Error should be row's length error")
+	}
+}
